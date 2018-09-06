2565ebc99eaad8219d43d7034e674815805cc628
Trystan Eveno <trystan.eveno@lephare.com>
2018-09-06 10:10:13 +0200

    generate fake lines

--- src/react/pages/Home.js
+++ src/react/pages/Home.js
@@ -1,27 +1,64 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react';
 
 import Container from '../components/Container';
 
 class Home extends Component {
   constructor(props) {
     super(props);
+
+    const template = this.preRendering(),
+      lines = this.generateFakeLineNumber(template);
+
+    // binding
+    this.generateFakeLineNumber   = this.generateFakeLineNumber.bind(this);
+    this.preRendering             = this.preRendering.bind(this);
+
+    this.state = {
+      lines     : lines,
+      template  : template
+    };
+  }
+
+  generateFakeLineNumber(template) {
+    const nb_lines = Math.ceil((document.documentElement.offsetHeight - 80) / 16) - template.props.children.length - 2;
+    let lines_generate = [];
+
+    for (let i = 0; i < nb_lines; i++) {
+      lines_generate.push(
+        <span key={ 'line-'+ i }>&nbsp;</span>
+      );
+    }
+
+    return lines_generate;
+  }
+
+  preRendering() {
+    return (
+      <Fragment>
+        <span>a</span>
+        <span>z</span>
+        <span>e</span>
+        <span>r</span>
+        <span>t</span>
+        <span>y</span>
+        <span>u</span>
+        <span>i</span>
+        <span>o</span>
+        <span>p</span>
+      </Fragment>
+    );
   }
 
   render() {
+    const lines = this.state.lines,
+      template = this.state.template;
+
     return (
       <Container>
         <div className="content code">
           <pre>
-            <span>a</span>
-            <span>z</span>
-            <span>e</span>
-            <span>r</span>
-            <span>t</span>
-            <span>y</span>
-            <span>u</span>
-            <span>i</span>
-            <span>o</span>
-            <span>p</span>
+            { template }
+            { lines }
           </pre>
         </div>
       </Container>
